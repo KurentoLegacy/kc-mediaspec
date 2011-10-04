5dc170a754a94ae22726093906b8e1c16dedb29c
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-04 17:01:23 +0200

    Implement base methods and constructors.

--- src/main/java/com/kurento/commons/media/format/formatparameters/impl/FormatParametersBase.java
+++ src/main/java/com/kurento/commons/media/format/formatparameters/impl/FormatParametersBase.java
@@ -4,10 +4,19 @@ import com.kurento.commons.media.format.formatparameters.FormatParameters;
 
 public abstract class FormatParametersBase implements FormatParameters {
 
+	protected String formatParamsStr;
+
+	public FormatParametersBase() {
+
+	}
+
+	public FormatParametersBase(String formatParamsStr) {
+		this.formatParamsStr = formatParamsStr;
+	}
+
 	@Override
 	public String getFormatParamsStr() {
-		// TODO Auto-generated method stub
-		return null;
+		return formatParamsStr;
 	}
 
 	@Override
