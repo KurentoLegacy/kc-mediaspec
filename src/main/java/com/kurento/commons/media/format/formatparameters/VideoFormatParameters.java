1f62a10ff9b47e1f8f53f547144df373d9ff7505
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-04 17:00:47 +0200

    Delete not generic methods

--- src/main/java/com/kurento/commons/media/format/formatparameters/VideoFormatParameters.java
+++ src/main/java/com/kurento/commons/media/format/formatparameters/VideoFormatParameters.java
@@ -1,15 +1,8 @@
 package com.kurento.commons.media.format.formatparameters;
 
-
 /**
  * Subinterface to manage a=fmtp attribute for video payloads
  */
 public interface VideoFormatParameters extends FormatParameters {
 
-	public int getWidth();
-
-	public int getHeight();
-
-	public int getFrameRate();
-	
 }
