9ee84517983973e6322e674026ab9448ad6a4d20
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-07 13:52:58 +0200

    Add Factory to create specific FormatParameters

---
+++ src/main/java/com/kurento/commons/media/format/formatparameters/FormatParametersFactory.java
@@ -0,0 +1,38 @@
+package com.kurento.commons.media.format.formatparameters;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.sdp.SdpException;
+
+import com.kurento.commons.media.format.formatparameters.impl.FormatParametersBase;
+import com.kurento.commons.media.format.formatparameters.impl.GenericFormatParameters;
+import com.kurento.commons.media.format.formatparameters.impl.H263FormatParameters;
+
+public class FormatParametersFactory {
+
+	private enum CodecStrings {
+		H263(new String[] { "H263", "H263-1998", "H263-2000" });
+		private Collection<String> collectionCodecStrings;
+
+		private CodecStrings(String[] codecStrings) {
+			collectionCodecStrings = new ArrayList<String>();
+			for (String s : codecStrings)
+				collectionCodecStrings.add(s);
+		}
+
+		public Collection<String> getCollectionCodecStrings() {
+			return collectionCodecStrings;
+		}
+	}
+
+	public static FormatParameters createFormatParameters(String encodingName,
+			String formatParamsStr) throws SdpException {
+		if (encodingName == null || encodingName.equals(""))
+			return null;
+		if (CodecStrings.H263.getCollectionCodecStrings().contains(
+				encodingName.toUpperCase()))
+			return new H263FormatParameters(formatParamsStr);
+		return new GenericFormatParameters(formatParamsStr);
+	}
+}
