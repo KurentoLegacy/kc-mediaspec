68b2ec19c163fb16b303ed02db9f638ea4d232da
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-07 13:50:17 +0200

    Add Generic class to unknow specify codec format parameters.

---
+++ src/main/java/com/kurento/commons/media/format/formatparameters/impl/GenericFormatParameters.java
@@ -0,0 +1,23 @@
+package com.kurento.commons.media.format.formatparameters.impl;
+
+import javax.sdp.SdpException;
+
+import com.kurento.commons.media.format.formatparameters.FormatParameters;
+
+public class GenericFormatParameters extends FormatParametersBase {
+
+	public GenericFormatParameters(String formatParamsStr) {
+		super(formatParamsStr);
+	}
+
+	@Override
+	public FormatParameters intersect(FormatParameters other)
+			throws SdpException {
+		return null;
+	}
+
+	@Override
+	public int compareTo(FormatParameters o) {
+		return -1;
+	}
+}
