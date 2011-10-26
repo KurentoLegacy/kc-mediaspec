dd58d955e5a321c8e149738587c132874bf07a0c
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-26 11:34:16 +0200

    Add MPEG4FormatParameters class.

---
+++ src/main/java/com/kurento/commons/media/format/formatparameters/impl/MPEG4FormatParameters.java
@@ -0,0 +1,22 @@
+package com.kurento.commons.media.format.formatparameters.impl;
+
+import javax.sdp.SdpException;
+
+import com.kurento.commons.media.format.formatparameters.FormatParameters;
+
+public class MPEG4FormatParameters extends VideoFormatParametersBase {
+
+	@Override
+	public FormatParameters intersect(FormatParameters other)
+			throws SdpException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(FormatParameters other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
