363dcefe04a1acd5be2249935b7c227e9f2ea721
Miguel París Díaz <mparisdiaz@gmail.com>
2011-10-28 12:11:17 +0200

    Add test for MPEG4FormatParameters.

---
+++ src/test/java/com/kurento/commons/media/format/MPEG4FormatParametersTest.java
@@ -0,0 +1,72 @@
+package com.kurento.commons.media.format;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import com.kurento.commons.media.format.formatparameters.impl.H263FormatParameters;
+import com.kurento.commons.media.format.formatparameters.impl.MPEG4FormatParameters;
+
+public class MPEG4FormatParametersTest extends TestCase {
+
+	public void testInit() throws Exception {
+		PatternLayout layout = new PatternLayout("[%c{1}.%M:%L]-%-5p - %m%n");
+		Logger logger = Logger.getRootLogger();
+		logger.addAppender(new ConsoleAppender(layout));
+		logger.setLevel(Level.TRACE);
+
+		createFromString();
+	}
+	
+	public void createFromString() throws Exception {
+		System.out.println("createFromString");
+		
+		
+		String fmtpStr = "profile-level-id=1; config=000001B001000001B58913000001000000012000C48D8AEE050B04241463000001B24C61766335322E37322E32";
+		MPEG4FormatParameters mpeg4fpFromStr = new MPEG4FormatParameters(fmtpStr);
+		System.out.println(mpeg4fpFromStr);
+		System.out.println("profile-level: " + mpeg4fpFromStr.getProfileLevel() );
+		System.out.println("width: " +  mpeg4fpFromStr.getVideoProfile().getWidth() );
+		System.out.println("height: " +  mpeg4fpFromStr.getVideoProfile().getHeight() );
+		System.out.println("frame rate: " +  mpeg4fpFromStr.getVideoProfile().getMaxFrameRate() );
+		
+		System.out.println("\n\n");
+		String fmtpStr2 = "profile-level-id=1; config=000001B001000001B5090000010000000120008440FA282C2090A21F";
+		MPEG4FormatParameters mpeg4fpFromStr2 = new MPEG4FormatParameters(fmtpStr2);
+		System.out.println(mpeg4fpFromStr2);
+		System.out.println("profile-level: " + mpeg4fpFromStr2.getProfileLevel() );
+		System.out.println("width: " +  mpeg4fpFromStr2.getVideoProfile().getWidth() );
+		System.out.println("height: " +  mpeg4fpFromStr2.getVideoProfile().getHeight() );
+		System.out.println("frame rate: " +  mpeg4fpFromStr2.getVideoProfile().getMaxFrameRate() );
+		
+		System.out.println("\n\n");
+		String fmtpStr3 = "profile-level-id=1; config=000001b009000001b509000001000000012000845d4c282c2090a28f";
+		MPEG4FormatParameters mpeg4fpFromStr3 = new MPEG4FormatParameters(fmtpStr3);
+		System.out.println(mpeg4fpFromStr3);
+		System.out.println("profile-level: " + mpeg4fpFromStr3.getProfileLevel() );
+		System.out.println("width: " +  mpeg4fpFromStr3.getVideoProfile().getWidth() );
+		System.out.println("height: " +  mpeg4fpFromStr3.getVideoProfile().getHeight() );
+		System.out.println("frame rate: " +  mpeg4fpFromStr3.getVideoProfile().getMaxFrameRate() );
+		
+		System.out.println("\n\n");
+		String fmtpStr4 = "profile-level-id=1; config=000001b009000001b509000001000000012000c488800c50604121463f";
+		MPEG4FormatParameters mpeg4fpFromStr4 = new MPEG4FormatParameters(fmtpStr4);
+		System.out.println(mpeg4fpFromStr4);
+		System.out.println("profile-level: " + mpeg4fpFromStr4.getProfileLevel() );
+		System.out.println("width: " +  mpeg4fpFromStr4.getVideoProfile().getWidth() );
+		System.out.println("height: " +  mpeg4fpFromStr4.getVideoProfile().getHeight() );
+		System.out.println("frame rate: " +  mpeg4fpFromStr4.getVideoProfile().getMaxFrameRate() );
+		
+		System.out.println("\n\n");
+		String fmtpStr5 = "profile-level-id=1; config=000001B001000001B58913000001000000012000C4F8404D8AEE050F04281463000001B24C61766335322E37322E32";
+		MPEG4FormatParameters mpeg4fpFromStr5 = new MPEG4FormatParameters(fmtpStr5);
+		System.out.println(mpeg4fpFromStr5);
+		System.out.println("profile-level: " + mpeg4fpFromStr5.getProfileLevel() );
+		System.out.println("width: " +  mpeg4fpFromStr5.getVideoProfile().getWidth() );
+		System.out.println("height: " +  mpeg4fpFromStr5.getVideoProfile().getHeight() );
+		System.out.println("frame rate: " +  mpeg4fpFromStr5.getVideoProfile().getMaxFrameRate() );
+	}
+}
