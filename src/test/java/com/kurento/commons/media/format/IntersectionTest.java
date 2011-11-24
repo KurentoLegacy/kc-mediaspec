b8a83ad0d60e879e4f49fcc7a6832e322b7c3ea2
Miguel París Díaz <mparisdiaz@gmail.com>
2011-11-24 12:02:52 +0100

    Update test with RTMP.

--- src/test/java/com/kurento/commons/media/format/IntersectionTest.java
+++ src/test/java/com/kurento/commons/media/format/IntersectionTest.java
@@ -55,6 +55,23 @@ public class IntersectionTest extends TestCase {
 			"a=FMTP:100 octet-align=1\r\n" +
 			"a=sendonly\r\n";
 	
+	private static String sdp3 = "v=0\n\r" +
+			"o=- 783800654 0 IN IP4 193.147.51.27\n\r" +
+			"s=http://www.portsip.com\n\r" +
+			"c=IN IP4 193.147.51.27\n\r" +
+			"t=0 0\n\r" +
+			"m=null 0 RTP/AVP 96\n\r" +
+			"a=rtpmap:96 RTMP/90000\n\r" +
+			"a=FMTP:96 url=rtmp://myserver.com/application;offerer=publishStream;answerer=playStream;w=320;h=240;fps=15/1\n\r";
+
+	private static String sdp4 = "v=0\n\r" +
+			"o=- 783800654 0 IN IP4 193.147.51.27\n\r" +
+			"s=http://www.portsip.com\n\r" +
+			"c=IN IP4 193.147.51.27\n\r" +
+			"t=0 0\n\r" +
+			"m=null 0 RTP/AVP 96\n\r" +
+			"a=rtpmap:96 RTMP/90000\n\r" +
+			"a=FMTP:96 url=rtmp://myserver.com/application2;offerer=publishStream2;answerer=playStream2;w=320;h=240;fps=15/1\n\r";
 	
 	private static String sdpIntersectLocal = "v=0\r\n" +
 			"o=- 123456 654321 IN IP4 193.147.51.44\r\n" +
@@ -68,7 +85,7 @@ public class IntersectionTest extends TestCase {
 			"a=rtpmap:100 AMR/8000/1\r\n" +
 			"a=FMTP:100 octet-align=1\r\n" +
 			"a=inactive\r\n";
-	
+
 	private static String sdpIntersectRemote = "v=0\r\n" +
 			"o=- 123456 654321 IN IP4 193.147.51.18\r\n" +
 			"s=TestSession\r\n" +
@@ -81,8 +98,6 @@ public class IntersectionTest extends TestCase {
 			"a=rtpmap:100 AMR/8000/1\r\n" +
 			"a=FMTP:100 octet-align=1\r\n" +
 			"a=inactive\r\n";
-	
-	
 
 	public void testInit() throws Exception {
 		PatternLayout layout = new PatternLayout("[%c{1}.%M:%L]-%-5p - %m%n");
@@ -93,7 +108,7 @@ public class IntersectionTest extends TestCase {
 		try {
 			SessionSpec spec = new SessionSpec(sdp);
 			SessionSpec spec2 = new SessionSpec(sdp2);
-			
+
 			SessionSpec intersect[] = SpecTools.intersectSessionSpec(spec2, spec);
 
 			System.out.println("---------------------------------");
@@ -106,6 +121,28 @@ public class IntersectionTest extends TestCase {
 			System.out.println("---------------------------------");
 			System.out.println(intersect[1].toString());
 			assertEquals(sdpIntersectRemote, intersect[1].toString());
+			System.out.println("---------------------------------");
+		} catch (SdpException e) {
+			e.printStackTrace();
+			throw e;
+		}
+
+		try {
+			SessionSpec spec3 = new SessionSpec(sdp3);
+			SessionSpec spec4 = new SessionSpec(sdp4);
+			System.out.println("Created OK");
+			SessionSpec intersect[] = SpecTools.intersectSessionSpec(spec4, spec3);
+
+			System.out.println("---------------------------------");
+			System.out.println(spec3.toString());
+			System.out.println("---------------------------------");
+			System.out.println(spec4.toString());
+			System.out.println("---------------------------------");
+			System.out.println(intersect[0].toString());
+			// assertEquals(sdpIntersectLocal, intersect[0].toString());
+			System.out.println("---------------------------------");
+			System.out.println(intersect[1].toString());
+			// assertEquals(sdpIntersectRemote, intersect[1].toString());
 			System.out.println("---------------------------------");
 		} catch (SdpException e) {
 			e.printStackTrace();
